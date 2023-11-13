5dcdf43923b1eb4e6a99d8ba15478c633e1b410a
AlexNik <bonika-nn@mail.ru>
2023-11-14 00:45:14 +0300

    переименовал класс в RandomGame

--- src/RandomGame.java
+++ src/RandomGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Start {
+public class RandomGame {
     public static void main(String[] args) {
         Random random = new Random();
         int num = random.nextInt(101);
